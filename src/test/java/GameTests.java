4e0623c55fcb7729a0f4ccc336e826201b0ce5b6
Pellizzaro Federico <67149530+Fedrosauro@users.noreply.github.com>
2022-12-08 16:21:47 +0100

    added costructor in tile and wall classes, started projecting the test in the GameTests.java

--- src/test/java/GameTests.java
+++ src/test/java/GameTests.java
@@ -1,7 +1,27 @@
 import org.junit.jupiter.api.Test;
+import quoridor.components.Tile;
+import quoridor.components.Wall;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTests {
 
-    
+    @Test
+    void placementWall(){
+        /*boolean correctPlace = false;
+
+        ArrayList<Tile> tileArrayList = new ArrayList<>();
+
+        tileArrayList.add(new Tile());
+        tileArrayList.add();
+        tileArrayList.add();
+        tileArrayList.add();
+
+
+        Wall w1 = new Wall();
+        assertTrue();*/
+    }
 
 }
