f485f6e1698276cb9a84b463b6b83fcd754185ba
Giacomo Vassallo <51168109+GiecV@users.noreply.github.com>
2022-12-10 18:45:09 +0100

    Deleted dummy orientation attribute in wall

--- src/main/java/quoridor/components/Wall.java
+++ src/main/java/quoridor/components/Wall.java
@@ -1,17 +1,8 @@
 package quoridor.components;
 
-import quoridor.utils.Orientation;
-
-import java.util.ArrayList;
-
 public class Wall {
 
-    private Orientation orientation;
-    private int dimension;
-
-    public Wall(){ //dummy
-        orientation = Orientation.HORIZONTAL;
-        dimension = 1;
+    public Wall() { //dummy
     }
 
 
