2d78b20a8bc2cd8cf484ca24610d23e5f55f7665
Giacomo Vassallo <51168109+GiecV@users.noreply.github.com>
2022-12-11 11:01:20 +0100

    Added tests and methods for checking winning condition

--- src/main/java/quoridor/game/Player.java
+++ src/main/java/quoridor/game/Player.java
@@ -9,4 +9,13 @@ public class Player {
     private int walls;
 
 
+    public Player(String name, Meeple meeple, int walls) { //dummy
+        this.name = name;
+        this.meeple = meeple;
+        this.walls = walls;
+    }
+
+    public Meeple getMeeple() {
+        return meeple;
+    }
 }
